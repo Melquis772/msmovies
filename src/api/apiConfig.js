ecdd020e2e76a6d2bf82441e840354c88537362c
Melquisedec Sepúlveda <melquis772@gmail.com>
2022-12-21 10:17:33 -0400

    switched backend hosting to railway.app

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://msmoviesapi.herokuapp.com',
+    baseUrl: 'https://msmovies-backend-production.up.railway.app',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
