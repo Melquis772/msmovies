d1fd297b429e19bc2a2681f71a0f5753eafd9bf1
Melquisedec Sepúlveda <melquis772@gmail.com>
2023-08-19 12:27:16 -0400

    Movies backend api link updated

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://msmovies-backend-production.up.railway.app',
+    baseUrl: 'https://ms-movies-backend.onrender.com',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
